5d1d287afe083120a8a45b03a7127f9fb43d4173
ADMIN <ADMIN@DESKTOP-B547FHT>
2020-12-07 17:47:25 +0700

    no_fit_polygon

--- no_fit_polygon_py3/README.md
+++
@@ -1,2 +0,0 @@
-more information from **SVGNest**: A browser-based vector nesting tool.
-[nesting](https://github.com/Jack000/SVGnest)
--- no_fit_polygon_py3/test/point_to_list.py
+++
@@ -1,9 +0,0 @@
-pointA =[{'x': 3000, 'y': 1400}, {'x': 0, 'y': 1400}, {'x': 0, 'y': 0}, {'x': 3000, 'y': 0}]
-
-pointB = [{'x': 1168.0, 'y': 2.0}, {'x': 2440.0, 'y': 2.0}, {'x': 2440.0, 'y': 668.0}, {'x': 1168.0, 'y': 668.0}]
-
-new_pointA = [[p['x'], p['y']] for p in pointA]
-new_pointB = [[p['x'], p['y']] for p in pointB]
-
-print(new_pointA)
-print(new_pointB)
